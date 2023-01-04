30781d5a55dbbb9993cbd70859356474dff5356d
Oleg Balunenko <oleg.balunenko@gmail.com>
2023-01-04 17:20:03 +0300

    fix: Coverage script

--- scripts/tests/coverage.sh
+++ scripts/tests/coverage.sh
@@ -2,26 +2,23 @@
 
 set -Eeuo pipefail
 
-function cleanup() {
-  trap - SIGINT SIGTERM ERR EXIT
-  echo "cleanup running"
-  #rm -rf coverage*.out.tmp
-}
+SCRIPT_NAME="$(basename "$0")"
+SCRIPT_DIR="$(dirname "$0")"
+REPO_ROOT="$(cd "${SCRIPT_DIR}" && git rev-parse --show-toplevel)"
+SCRIPTS_DIR="${REPO_ROOT}/scripts"
+COVER_DIR=${REPO_ROOT}/coverage
 
-trap cleanup SIGINT SIGTERM ERR EXIT
-
-SCRIPT_NAME="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
-REPO_ROOT="$(cd ${SCRIPT_DIR} && git rev-parse --show-toplevel)"
+source "${SCRIPTS_DIR}/helpers-source.sh"
 
 echo "${SCRIPT_NAME} is running... "
 
-rm -rf coverage
-mkdir -p coverage
+checkInstalled gocov
+
+rm -rf "${COVER_DIR}"
+mkdir -p "${COVER_DIR}"
 
 go test --count=1 -tags=integration_test -coverprofile ./coverage/integration.cov -covermode=atomic ./...
 go test --count=1 -coverprofile ./coverage/unit.cov -covermode=atomic ./...
-
 
 {
 echo "mode: atomic"
