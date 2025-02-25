e32d18753fe7236f0e41ffd67193e3f89ed9b226
Oleg Balunenko <oleg.balunenko@gmail.com>
2025-02-26 02:05:32 +0400

    feat: Bump go to 1.24; Bump exiftool to 13.21 (#175)
    * chore: Bump go to 1.24
    
    * feat: Bump exiftool to 13.21
    
    * fix: ci configs

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -19,23 +19,4 @@ APP=${APP_NAME}
 
 echo "${SCRIPT_NAME} is running fo ${APP}... "
 
-COMMIT="$(git rev-parse HEAD)"
-SHORTCOMMIT="$(git rev-parse --short HEAD)"
-DATE="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
-VERSION="$(git tag | sort -V | tail -1)"
-GOVERSION="$(go version | awk '{print $3;}')"
-
-if [ -z "${VERSION}" ] || [ "${VERSION}" = "${SHORTCOMMIT}" ]; then
-  VERSION="v0.0.0"
-fi
-
-BUILDINFO_VARS_PKG=github.com/obalunenko/version
-export GO_BUILD_LDFLAGS="-s -w \
--X ${BUILDINFO_VARS_PKG}.version=${VERSION} \
--X ${BUILDINFO_VARS_PKG}.commit=${COMMIT} \
--X ${BUILDINFO_VARS_PKG}.shortcommit=${SHORTCOMMIT} \
--X ${BUILDINFO_VARS_PKG}.builddate=${DATE} \
--X ${BUILDINFO_VARS_PKG}.appname=${APP} \
--X ${BUILDINFO_VARS_PKG}.goversion=${GOVERSION}"
-
 goreleaser release --clean
